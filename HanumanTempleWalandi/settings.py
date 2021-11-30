69e349396f8ab0b11155e0921b34c562fc06deca
sangmesh04 <78370367+sangmesh04@users.noreply.github.com>
2021-11-30 18:31:40 +0530

    allowed urls

--- HanumanTempleWalandi/settings.py
+++ HanumanTempleWalandi/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7wjdm&6)0_+0f2lgdqp&&0nh(bua(4xa)9g$)nndesex=0f6m^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hanumantemplewalandi.herokuapp.com','127.0.0.1']
 
 
 # Application definition
