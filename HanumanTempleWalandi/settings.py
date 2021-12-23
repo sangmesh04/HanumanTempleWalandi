998055826a77644e42d7deb406c7bcbf7897c716
sangmesh04 <78370367+sangmesh04@users.noreply.github.com>
2021-12-23 16:33:59 +0530

    debug false

--- HanumanTempleWalandi/settings.py
+++ HanumanTempleWalandi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7wjdm&6)0_+0f2lgdqp&&0nh(bua(4xa)9g$)nndesex=0f6m^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost",  "127.0.0.1", "hanumantemplewalandi.herokuapp.com"]
 
